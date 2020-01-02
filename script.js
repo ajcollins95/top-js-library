4a6d1428b0d824bd08f5c942e992f492f86e5f84
Aaron Collins <aa.collins.95@gmail.com>
2020-01-02 17:44:33 -0500

    Can add book to library

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function init() {
+    //initializes library data
     let myLibrary = localStorage.getItem("myLibrary")
+    document.getElementsByTagName('form')[0].addEventListener('submit', onSubmit)
+
     // first time loaded!
     if(!myLibrary) {
         console.log('First Time!')
         let myLibrary = []
         localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
 
-        let a = new Book('East of Eden', 'John Steinbeck', 394, 'y')
-        let b = new Book('Surprised by Joy', 'C.S. Lewis', 182, 'n')
-        let c = new Book('Man\'s Search for Meaning', 'Viktor Frankl', 246, 'y')
+        let a = new Book('East of Eden', 'John Steinbeck', 394, 'on')
+        let b = new Book('Surprised by Joy', 'C.S. Lewis', 182, 'off')
+        let c = new Book('Man\'s Search for Meaning', 'Viktor Frankl', 246, 'on')
 
         addBookToLibrary(a)
         addBookToLibrary(b)
@@ -18,10 +21,11 @@ function init() {
 }
 
 function Book(title, author, pages, read) {
+    //Book object prototype
     this.title = title
     this.author = author
     this.pages = pages 
-    this.read = read
+    this.read = read == 'on' ? 'Yes' : 'No'
     //this.index = 0
 
     this.info = function() {
@@ -34,37 +38,18 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     //adds a book to the library
     let storedLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-    console.log(storedLibrary)
-    alert('I stopped')
     book.index = storedLibrary.length
     storedLibrary.push(book);
     console.log(book.index)
     localStorage.setItem("myLibrary",JSON.stringify(storedLibrary))
 }
 
-function makefarmRow(book) {
-    //make new row about a book
-    var row = document.createElement('tr')
-
-    Object.keys(book).forEach(function (key) {
-        //don't add anything for the info function
-        if (key != 'info') {
-            //console.log(key)
-            var col = document.createElement('td')
-            col.textContent = book[key]
-            row.appendChild(col)
-        }
-    })
-    return row;
-}
-
 function makeBookRow(book) {
-    //make new row
+    //make new row of book data
     var row = document.createElement('tr')
     let validKeys = ['title','author','pages','read']
 
     validKeys.forEach(function (key) {
-        //console.log(key)
         var col = document.createElement('td')
         col.textContent = book[key]
         row.appendChild(col)
@@ -72,25 +57,9 @@ function makeBookRow(book) {
     })
     return row;
 }
-/*
-
-document.getElementById('submit').addEventListener('click', submission)
-
-function submission() {
-    //Adds book on submission
-    var formElement = document.getElementById("myForm");
-    let data = new FormData(formElement);
-    let book = new Book(data.get('title'), 
-                        data.get('author'), 
-                        data.get('pages'), 
-                        data.get('read'))
-    addBookToLibrary(book)
-    console.log(myLibrary)
-    alert(book.title)
-}
-*/
 
 function render() {
+    //renders data to html for display
     let parent = document.getElementById('table')
     let storedLibrary = JSON.parse(localStorage.getItem("myLibrary"));
     //var newBook = document.getElementById('btn')
@@ -102,21 +71,32 @@ function render() {
     
 };
 
+function validSubmission(inputs) {
+    return 1
+}
 
-init()
-
-//console.log(myLibrary[0]['title'])
-
-render()
-setup()
-//document.getElementById('submit').addEventListener('click', submission)
-
-function setup() {
+function onSubmit() {
+    
     let form = document.getElementsByTagName('form')[0]
-    let inputs = document.getElementsByTagName('inputs')[0]
+    let inputs = document.getElementsByTagName('input')
+    //console.log(window.location.href)
     console.log(inputs)
+    if (validSubmission(inputs)) {
+        let title = inputs[0]['value'], author = inputs[1]['value'],
+            pages = inputs[2]['value'], read = inputs[3]['value'];
+            addBookToLibrary(new Book(title, author, pages, read))
+    }
+    console.log('setting up...')
+    
 
 }
+
+
+init()
+render()
+
+
+
 /*
 function submission() {
     //Adds book on submission
